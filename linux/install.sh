5d4a1b8b24326a14d81b509a13cc0c6216c3106a
Shawn Rigdon <srigdon@isi.edu>
2022-07-31 15:09:14 -0400

    adding alias completion support

--- linux/install.sh
+++ linux/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install bash-completion
+git submodule update --init --recursive
+
 cp -r nvim ~/.config/
 cp .bash* ~/
+cp complete-alias/complete_alias ~/.complete_alias
