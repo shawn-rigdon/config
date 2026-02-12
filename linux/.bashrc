2457948ef746f6ba3995912138d1aa7b40004c6a
Shawn Rigdon <shawn.patrick.rigdon@gmail.com>
2026-02-12 10:33:37 -0500

    switching to nvm for node installation

--- linux/.bashrc
+++ linux/.bashrc
@@ -157,3 +157,7 @@ export NDKROOT="/home/srigdon/Android/Sdk/ndk/25.1.8937393"
 export NDK_ROOT="/home/srigdon/Android/Sdk/ndk/25.1.8937393"
 . "$HOME/.cargo/env"
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+function source_nvm {
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
 git submodule update --init --recursive
+sudo apt-get install curl
 
 # bash aliases
 echo "Updating bash aliases with completion ..."
@@ -9,28 +16,32 @@ git submodule update --init --recursive
 
 cp .bash* ~/
 cp complete-alias/complete_alias ~/.complete_alias
+source_nvm
 
 # neovim installation
 if ! which nvim &> /dev/null; then
 	echo "Building neovim from source ..."
-	sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+	sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip doxygen
 	pushd neovim
 	make CMAKE_BUILD_TYPE=RelWithDebInfo
 	sudo make install 
 	popd
 fi
 
-if ! which node &> /dev/null; then
-	curl -sL install-node.now.sh | sudo bash #install latest stable nodejs
-	curl --compressed -o- -L https://yarnpkg.com/install.sh | sudo bash #install yarn
+if ! command -v nvm &> /dev/null; then
+	echo "Installing nvm (node version manager) ..."
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+	source_nvm
+	echo "Installing latest stable nodejs and yarn ..."
+	nvm install node
+	npm install -g yarn
 fi
 
 echo "Installing neovim configuration ..."
-sudo apt-get install xclip curl
+sudo apt-get install xclip
 cp -r nvim ~/.config/
 cp -r nerdfonts ~/.local/share/fonts/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-. ~/.bashrc
 sudo npm i -g pyright
 nvim --headless +PlugInstall +qall
